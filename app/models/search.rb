cdf5a59bd422757b4dd1ce10232c986007217fb5
Christian Sutter <git@doublesignal.com>
2023-11-16 15:42:22 +0000

    Render some placeholder results

--- app/models/search.rb
+++ app/models/search.rb
@@ -2,4 +2,20 @@ class Search
   include ActiveModel::Model
 
   attr_accessor :query
+
+  def results
+    [
+      {
+        link: {
+          text: "Test result for #{query}",
+          path: "/test",
+          description: "Lorem ipsum dolor sit amet.",
+        },
+        metadata: {
+          public_updated_at: Time.parse("2021-11-25T10:15:49.000+00:00"),
+          document_type: "travel_advice",
+        },
+      },
+    ] * 5
+  end
 end
