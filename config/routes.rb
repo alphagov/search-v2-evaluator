95543c1fcf00973ec2e39546473e505eb10854e4
Christian Sutter <git@doublesignal.com>
2023-11-16 13:37:18 +0000

    Add placeholder search page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount GovukPublishingComponents::Engine, at: "/component-guide" if Rails.env.development?
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resource :search, only: %i[show]
+
+  root "searches#show"
 end
