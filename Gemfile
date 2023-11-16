159cab60236f3d1a1088061892e806ece84c8ad4
Christian Sutter <git@doublesignal.com>
2023-11-16 13:35:52 +0000

    Add publishing components

--- Gemfile
+++ Gemfile
@@ -5,8 +5,10 @@ ruby "3.2.2"
 gem "rails", "~> 7.1.2"
 
 gem "govuk_app_config"
+gem "govuk_publishing_components"
 
 gem "importmap-rails"
+gem "sassc-rails"
 gem "sprockets-rails"
 
 group :development, :test do
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  mount GovukPublishingComponents::Engine, at: "/component-guide" if Rails.env.development?
 
   # Defines the root path route ("/")
   # root "posts#index"
