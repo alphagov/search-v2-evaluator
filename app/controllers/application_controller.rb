4b31f166dd2a0bf3198a4f6f67c156b2dc98c7b2
Christian Sutter <git@doublesignal.com>
2023-11-20 10:13:57 +0000

    Enable basic auth when appropriate env present

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  if ENV["BASIC_AUTH_USERNAME"] && ENV["BASIC_AUTH_PASSWORD"]
+    http_basic_authenticate_with(
+      name: ENV.fetch("BASIC_AUTH_USERNAME"),
+      password: ENV.fetch("BASIC_AUTH_PASSWORD"),
+    )
+  end
 end
