ed1b49d6c310dc4e7d8da0572e54f491594f539a
Christian Sutter <git@doublesignal.com>
2023-11-26 15:04:12 +0000

    Add parts to results that have them

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,5 +1,9 @@
 class Result
   include ActiveModel::Model
 
-  attr_accessor :title, :description, :link, :content_id, :document_type, :public_timestamp
+  attr_accessor :title, :description, :link, :content_id, :document_type, :public_timestamp, :parts
+
+  def parts?
+    parts.any?
+  end
 end
