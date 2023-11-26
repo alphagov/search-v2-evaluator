1e04546c2cb8f950cb41b69de637dfd7c7b9f70b
Christian Sutter <git@doublesignal.com>
2023-11-26 14:53:22 +0000

    Use new cookie storage throughout

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -7,6 +7,16 @@ class SearchesController < ApplicationController
 private
 
   def query_params
-    params.permit(:query, search_options: [])
+    params.permit(:query).merge(count: show_more_results? ? 10 : 5)
   end
+
+  def show_more_results?
+    ActiveModel::Type::Boolean.new.cast(cookies[:show_more_results])
+  end
+  helper_method :show_more_results?
+
+  def show_metadata?
+    ActiveModel::Type::Boolean.new.cast(cookies[:show_metadata])
+  end
+  helper_method :show_metadata?
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,12 +8,4 @@ module ApplicationHelper
 
     link_to text, full_url, class: "govuk-link"
   end
-
-  def show_more_results?
-    ActiveModel::Type::Boolean.new.cast(cookies[:show_more_results])
-  end
-
-  def show_metadata?
-    ActiveModel::Type::Boolean.new.cast(cookies[:show_metadata])
-  end
 end
