7c752d8053c97c0768e283bf04f00492fcfc4184
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2024-07-31 09:54:40 +0100

    Fix breaking changes
    - details.js no longer exists

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,6 +2,5 @@
 //= require govuk_publishing_components/lib
 //= require govuk_publishing_components/components/button
 //= require govuk_publishing_components/components/checkboxes
-//= require govuk_publishing_components/components/details
 //= require govuk_publishing_components/components/layout-header
 //= require govuk_publishing_components/components/skip-link
